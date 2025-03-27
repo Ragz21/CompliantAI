4a120e81f346298eb0a9e6a9c10367f1169c0472
raghava-airoi <raghavs@airoi.com>
2025-03-27 16:51:48 -0700

    init

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from llms.esg import ESG
 from llms.rag import RAG
 
 def insert_docs(folder_path):
-    # folder_path = "/Users/ragz/Library/Mobile Documents/com~apple~CloudDocs/Freelance/AIROI/AI/output"
+    # folder_path = ""
     llm = RAG(config_path="config/config.yaml")
     llm.index_documents(folder=folder_path, use_esg=True)
 
@@ -10,7 +10,7 @@ def insert_docs(folder_path):
 
 def main():
 
-    # insert_docs("/Users/ragz/Library/Mobile Documents/com~apple~CloudDocs/Freelance/AIROI/AI/output")
+    # insert_docs("")
     lt = ESG(config_path="config/config.yaml")
     user_query = input("Enter your ESG query: ")
     response = lt.answer_query(user_query)
